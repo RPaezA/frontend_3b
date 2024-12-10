915b746a4cc7b6c51489e3a405993c21ae5d92ea
RPaezA <136133391+RPaezA@users.noreply.github.com>
2024-12-10 07:32:22 -0500

    clase 15

--- src/app/admin/inventario/components/categoria/categoria.component.ts
+++ src/app/admin/inventario/components/categoria/categoria.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CategoriaService } from '../../services/categoria.service';
+import { FormControl, FormGroup } from '@angular/forms';
+
+interface Categoria{
+  id?: number,
+  nombre: string,
+  detalle?:string
+}
 
 @Component({
   selector: 'app-categoria',
@@ -6,5 +14,40 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+  private categoriaService=inject(CategoriaService)
+  categorias:Categoria[]=[]
+  dialog_visible: boolean=false
+  categoriaForm=new FormGroup({
+    nombre:new FormControl(''),
+    detalle: new FormControl('')
+  })
+  ngOnInit():void{
+    this.getCategorias()
+  }
+  getCategorias(){
+    this.categoriaService.funListar().subscribe(
+      (res:any)=>{
+        this.categorias=res;
+      },
+      (error:any)=>{
+        console.log(error)
+      }
+    )
+  }
+  mostrarDialogo(){
+    this.dialog_visible=true
+  }
 
+  guardarCategoria(){
+    this.categoriaService.funGuardar(this.categoriaForm.value).subscribe(
+      (res:any)=>{
+        this.dialog_visible=false;
+        this.getCategorias()
+
+      },
+      (error:any)=>{
+        console.log(error)
+      }
+    )
+  }
 }
